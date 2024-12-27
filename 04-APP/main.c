663009182663c688d3d0d3fcfbc81207f08edee4
helmy001 <mohammed20191700575@cis.asu.edu.eg>
2024-12-27 16:39:14 +0200

    Tesing NVIC Driver with Priority by Interrupt Preemption

--- 04-APP/main.c
+++ 04-APP/main.c
@@ -5,40 +5,57 @@
 #include "..\02-MCAL\03-NVIC\NVIC_interface.h"
 #include "..\02-MCAL\04-EXTI\EXTI_interface.h"
 #include "..\02-MCAL\05-AFIO\AFIO_interface.h"
+#include "..\02-MCAL\06-SYSTICK\SYSTICK_interface.h"
 
 
 int main(void)
 {
     RCC_Init();
     
+    RCC_voidPeripheralClockEnable(APB2_PERIPHERAL_BUS,RCC_APB2_IOPAEN);
     RCC_voidPeripheralClockEnable(APB2_PERIPHERAL_BUS,RCC_APB2_IOPBEN);
     RCC_voidPeripheralClockEnable(APB2_PERIPHERAL_BUS,RCC_APB2_IOPCEN);
-    RCC_voidPeripheralClockEnable(APB2_PERIPHERAL_BUS,RCC_APB2_IOPAEN);
 
     GPIO_voidInitPinMode(GPIO_PORTB,GPIO_PIN14,GP_OUT_PUSH_PULL_2MHZ);
     GPIO_voidInitPinMode(GPIO_PORTC,GPIO_PIN13,GP_OUT_PUSH_PULL_2MHZ);
-    GPIO_voidInitPinMode(GPIO_PORTA,GPIO_PIN8,INPUT_PULL_DOWN);
+    NVIC_voidInit();
+    NVIC_voidSetInterruptPriority(EXTI0_INT,1,1);
+    NVIC_voidSetInterruptPriority(EXTI1_INT,0,0);
 
+    NVIC_voidEnableInterrupt(EXTI0_INT);
+    NVIC_voidEnableInterrupt(EXTI1_INT);
+
+    NVIC_voidSetPendingFlag(EXTI0_INT);
 
     while(1)
     {   
 
-
-        
-        // if(GPIO_u8ReadPinValue(GPIO_PORTA,GPIO_PIN8))
-        // {
-        //     GPIO_voidWritePin(GPIO_PORTB, GPIO_PIN14, GPIO_PIN_SET);
-        // }else
-        // {
-        //     GPIO_voidWritePin(GPIO_PORTB, GPIO_PIN14, GPIO_PIN_RESET);
-        // }
-    
-	    // GPIO_voidWritePin(GPIO_PORTC, GPIO_PIN13, GPIO_PIN_SET);
-	    // for(unsigned long i=0;i<1000000;i++);
-	    // GPIO_voidWritePin(GPIO_PORTC, GPIO_PIN13, GPIO_PIN_RESET);
-        // for(unsigned long i=0;i<1000000;i++);
-
-
     }
     return 0;
+}
+
+void EXTI0_IRQHandler()
+{
+    //NVIC_voidClearPendingFlag(EXTI0_INT);
+    NVIC_voidSetPendingFlag(EXTI1_INT);
+    while (1)
+    {
+        GPIO_voidWritePin(GPIO_PORTC, GPIO_PIN13, GPIO_PIN_SET);
+        for(unsigned long i=0;i<1000000;i++);
+        GPIO_voidWritePin(GPIO_PORTC, GPIO_PIN13, GPIO_PIN_RESET);
+        for(unsigned long i=0;i<1000000;i++);
+    }
+}    
+    
+
+
+void EXTI1_IRQHandler()
+{
+    while (1)
+    {
+        GPIO_voidWritePin(GPIO_PORTB, GPIO_PIN14, GPIO_PIN_SET);
+        for(unsigned long i=0;i<1000000;i++);
+        GPIO_voidWritePin(GPIO_PORTB, GPIO_PIN14, GPIO_PIN_RESET);
+        for(unsigned long i=0;i<1000000;i++);
+    }
 }
